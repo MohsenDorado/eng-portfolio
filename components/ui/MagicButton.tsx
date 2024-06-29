66d1280fe6d4deaa39283d69904666755afdc2d4
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-06-29 22:26:11 +0330

    quick type fix

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react'
 
-const MagicButton = ({title,icon,position,handleClick,otherClasses}:{title:string,icon:ReactNode,position:string,handleClick:()=>void,otherClasses?:string}) => {
+const MagicButton = ({title,icon,position,handleClick,otherClasses}:{title:string,icon:ReactNode,position:string,handleClick?:()=>void,otherClasses?:string}) => {
   return (
 <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] w-full  ">
   <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
