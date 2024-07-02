fb796dfa529dbea93a3360103d7947cf4e6686f0
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-07-02 17:16:32 +0330

    Sentry Added (Guess not working)

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -55,7 +55,7 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
+  const leftLists = ["ReactJS", "Expressjs", "Typescript"];
   const rightLists = ["Framer Motion","Nextjs", "Prisma" ];
 
   const [copied, setCopied] = useState(false);
@@ -79,7 +79,7 @@ export const BentoGridItem = ({
     <div
       className={cn(
         // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex  flex-col space-y-4",
         className
       )}
       style={{
